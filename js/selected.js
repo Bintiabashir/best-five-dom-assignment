dce9f12b274ad91e154288431639cd97aa9c9c45
Bintiabashir <bintia.bashir2121@gmail.com>
2022-08-31 01:54:39 +0600

    selected part done

---
+++ js/selected.js
@@ -0,0 +1,25 @@
+
+const nameArray = [];
+
+function display(selectedPlayersName) {
+    const tableBody = document.getElementById('listed-name');
+    tableBody.innerHTML = "";
+    for (i = 0; i < selectedPlayersName.length; i++) {
+        const name = nameArray[i];
+        // console.log(name)
+        const tr = document.createElement("tr");
+        tr.innerHTML = ` 
+        <th class="pr-3">${i + 1} </th>
+        <td>${name}</td>
+        `;
+        tableBody.appendChild(tr);
+    }
+
+}
+
+function selectPlayers(element) {
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    nameArray.push(playerName);
+    document.getElementById('total-added-players').innerText = nameArray.length;
+    display(nameArray);
+}
